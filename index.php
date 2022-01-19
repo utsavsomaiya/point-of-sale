30b1c13f4f18d2fb2126b86abcdaf74cd599301a
Utsav Somaiya <96036522+UtsavSomaiya@users.noreply.github.com>
2022-01-19 14:19:32 +0530

    Add Subtotal Module

--- index.php
+++ index.php
@@ -3,37 +3,13 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0ed3cf">
-    <meta name="msapplication-TileColor" content="#0ed3cf">
-    <meta name="theme-color" content="#0ed3cf">
-
-    <meta property="og:image" content="https://tailwindcomponents.com/storage/2190/temp35198.png?v=2022-01-04 10:47:18" />
-    <meta property="og:image:width" content="1280" />
-    <meta property="og:image:height" content="640" />
-    <meta property="og:image:type" content="image/png" />
-
-    <meta property="og:url" content="https://tailwindcomponents.com/component/point-of-sale-system/landing" />
-    <meta property="og:title" content="Dashboard Point of sale system by rahulsya" />
-    <meta property="og:description" content="template design from dribble https://dribbble.com/shots/14139155-Point-of-Sale-System-Design/attachments/5763503?mode=media" />
-
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@TwComponents" />
-    <meta name="twitter:title" content="Dashboard Point of sale system by rahulsya" />
-    <meta name="twitter:description" content="template design from dribble https://dribbble.com/shots/14139155-Point-of-Sale-System-Design/attachments/5763503?mode=media" />
-    <meta name="twitter:image" content="https://tailwindcomponents.com/storage/2190/temp35198.png?v=2022-01-04 10:47:18" />
 
     <title>Retail Shop</title>
 
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.0.2/dist/tailwind.min.css">
     <script type="text/javascript">
         const cart = [];
+        let subTotal = 0;
         function addToCart(id) {
           const product = {
             "id": id,
@@ -61,7 +37,7 @@
           });
         }
         function displayCart() {
-
+            subTotal = 0;
           document.getElementById('container').innerHTML = "";
           id = 1;
 
@@ -82,6 +58,8 @@
             var productName = cart[i].name;
             var productPrice = cart[i].price;
             var productQuantity = cart[i].quantity;
+
+            subTotal += parseInt((cart[i].price).slice(1));
 
             var imageTag = document.createElement('img');
             imageTag.src = productImg;
@@ -123,6 +101,8 @@
             divMainSubThird.setAttribute('id','div-sub-third-'+id);
             divMainSubThird.innerHTML = productPrice;
             document.getElementById('div-main-' + id).appendChild(divMainSubThird);
+
+            document.getElementById('subtotal').innerHTML = "$" + subTotal;
             id++;
           }
         }
@@ -172,7 +152,7 @@
                 <div class="font-bold text-gray-800" id="<?= "name-".$id; ?>"><?= $product["name"] ?></div>
                 <span class="font-light text-sm text-gray-400">
                 <?php
-                $fetch = $pdo->prepare("select name from category where id = {$product["category"]}");
+                $fetch = $pdo->prepare("select name from category where id = {$product["category_id"]}");
                         $fetch->execute();
                         $result = $fetch->fetchAll();
                         foreach ($result as $category) {
@@ -205,7 +185,7 @@
             <div class="py-4 rounded-md shadow-lg">
               <div class=" px-4 flex justify-between ">
                 <span class="font-semibold text-sm">Subtotal</span>
-                <span class="font-bold">$0</span>
+                <span class="font-bold" id="subtotal">$0</span>
               </div>
               <div class=" px-4 flex justify-between ">
                 <span class="font-semibold text-sm">Discount</span>
