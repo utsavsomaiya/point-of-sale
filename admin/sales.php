8009986ce7690c88281e9e9a46bfc1d893bdb025
Utsav Somaiya <96036522+UtsavSomaiya@users.noreply.github.com>
2022-01-29 13:09:18 +0530

    dispaly-sales
    we can show the sales table record

---
+++ admin/sales.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+include 'layout/header.php';
+?>
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-lg-6 grid-margin stretch-card">
+                <div class="card-body">
+                    <h4 class="card-title">
+                        <span style="margin-right:80px;">Sales Products</span>
+                    </h4>
+                    <table class="table">
+                        <form method="post">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Subtotal</th>
+                                    <th>Total_tax</th>
+                                    <th>Discount</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                require 'layout/db_connect.php';
+                                $fetch = $pdo->prepare('select * from sales');
+                                $fetch->execute();
+                                $result = $fetch->fetchAll();
+                                foreach ($result as $salesRecord) {
+                                    if (!empty($salesRecord)) {
+                                        ?>
+                                <tr>
+                                    <td><?= $salesRecord['id'] ?></td>
+                                    <td><?= $salesRecord['subtotal'] ?></td>
+                                    <td><?= $salesRecord['total_tax'] ?></td>
+                                    <td><?= $salesRecord['discount'] ?></td>
+                                    <td><?= $salesRecord['total']?></td>
+                                </tr>
+                                <?php
+                                    } else {
+                                        echo "No Record Found..";
+                                    }
+                                }
+                                ?>
+                            </tbody>
+                        </form>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'layout/footer.php';?>
