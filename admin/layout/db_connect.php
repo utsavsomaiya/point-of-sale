dcfc662b7c5296e684dc09259d060f6486cf7a5c
Utsav Somaiya <utsavsomaiya4464@gmail.com>
2022-01-31 13:31:09 +0530

    Update database name

--- admin/layout/db_connect.php
+++ admin/layout/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=abc', 'root', '1234');
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=point-of-sale', 'root', '1234');
 } catch (PDOException $e) {
     die($e->getMessage());
 }
