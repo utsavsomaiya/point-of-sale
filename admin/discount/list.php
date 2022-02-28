2e68b405c443d9b6e392f523c73a4a1432b29993
UtsavSomaiya <utsavsomaiya4464@gmail.com>
2022-02-28 13:08:09 +0530

    discount CRUD

--- admin/discount/list.php
+++ admin/discount/list.php
@@ -5,24 +5,24 @@ require '../layout/db_connect.php';
 <?php  include '../layout/header.php';?>
 <div class="main-panel">
     <div class="content-wrapper">
-            <div class="col-md-6 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">
+        <div class="col-md-6 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">
                         <span style="margin-right:80px;">Discounts</span>
                         <a href="add.php">Add New Discount</a>
                     </h4>
                     <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Digit</th>
-                                    <th>Status</th>
-                                    <th colspan='2'>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Digit</th>
+                                <th>Status</th>
+                                <th colspan='2'>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
                                 require '../layout/db_connect.php';
                                 $fetch = $pdo->prepare('SELECT * FROM `discount` ORDER BY `id` DESC');
                                 $fetch->execute();
@@ -30,52 +30,47 @@ require '../layout/db_connect.php';
                                 foreach ($result as $discount) {
                                     if (!empty($discount)) {
                                         ?>
-                                <tr>
-                                    <td><?= $discount['id'] ?></td>
-                                    <?php
+                            <tr>
+                                <td><?= $discount['id'] ?></td>
+                                <?php
                                         if ($discount['type'] == "1") {
                                             ?>
-                                    <td><?= $discount['digit']."%" ?></td>
-                                    <?php
-                                        } else { ?>
-                                        <td><?= "$".$discount['digit'] ?></td>
-                                        <?php } ?>
-                                    <td>
-                                        <?php
-                                        if ($discount['status'] == "1") {
-                                            ?>
-                                                <label class="switch">
-                                                    <input type="checkbox" id="toggle-<?=$discount['id']; ?>" name="toggle" value="<?=$discount['status']; ?>"  data-toggle="toggle" data-off="Disabled" data-on="Enabled">
-                                                    <span class="slider round"></span>
-                                                </label>
-                                    <?php
-                                        } else { ?>
-                                               <label class="switch">
-                                                    <input type="checkbox" id="toggle-<?=$discount['id']; ?>" name="toggle" value="<?=$discount['status']; ?>" checked
-                                                    data-toggle="toggle" data-off="Disabled" data-on="Enabled">
-                                                    <span class="slider round"></span>
-                                                </label>
-                                        <?php } ?>
-                                            </td>
-                                    <td><a href="../discount/edit.php?id=<?= $discount['id'] ?>"><img
-                                                src="/admin/image/edit-icon.png" /></a></td>
-                                    <td><a href="javascript:alert_d(<?= $discount['id'] ?>)"><i class="fa fa-trash-o" style="font-size:24px"></i></a>
-                                    </td>
-                                </tr>
+                                <td><?= $discount['digit']."%" ?></td>
                                 <?php
+                                        } else { ?>
+                                <td><?= "$".$discount['digit'] ?></td>
+                                <?php } ?>
+                                <td>
+                                    <label class="switch">
+                                        <input type="checkbox" value="<?=$discount['status']; ?>" <?php
+                                                    if ($discount['status'] == "2") {
+                                                        echo "checked";
+                                                    } ?> onclick="stateChanged(<?= $discount['id'] ; ?>,<?= $discount['status']?>)">
+                                        <span class="slider round"></span>
+                                    </label>
+                                </td>
+
+                                <td><a href="../discount/edit.php?id=<?= $discount['id'] ?>"><img
+                                            src="/admin/image/edit-icon.png" /></a></td>
+                                <td><a href="javascript:alert_d(<?= $discount['id'] ?>)"><i class="fa fa-trash-o"
+                                            style="font-size:24px"></i></a>
+                                </td>
+                            </tr>
+                            <?php
                                     } else {
                                         echo "No Record Found..";
                                     }
                                 }
                                 ?>
-                            </tbody>
+                        </tbody>
                         </form>
                     </table>
                 </div>
             </div>
         </div>
     </div>
-<?php
+    <script type="text/javascript" src="custom.js"></script>
+    <?php
     include '../layout/footer.php';
     unset($_SESSION['msg']);
 ?>
